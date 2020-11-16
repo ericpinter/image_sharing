30204bb30161de785e38fdbe55c22c64e9f9b3a8
Vi Nguyen <nguyenvv@hendrix.edu>
2020-11-16 00:47:04 -0600

    Add friend button
    Add a friend's ip address

--- lib/friends.dart
+++ lib/friends.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'send.dart';
+import 'friends_model.dart';
 
 class FriendsTab extends StatefulWidget {
   @override
@@ -7,9 +9,56 @@ class FriendsTab extends StatefulWidget {
 }
 
 class _FriendsTabState extends State<FriendsTab> {
-  @override
-  Widget build(BuildContext context) {
-    return Text("Friends");
+  TextEditingController ipController;
+
+  void initState() {
+    super.initState();
+    ipController = TextEditingController();
   }
 
+  void dispose() {
+    ipController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(padding: const EdgeInsets.all(8), children: [
+      RaisedButton(onPressed: _addFriend, child: Text("Add Friend")),
+      Text("Friends")
+    ]);
+  }
+
+  //Widget submitButton = FlatButton(child: Text("Add"), onPressed: () {});
+
+  //Widget cancelButton = FlatButton(child: Text("Cancel"), onPressed: () {});
+
+  _addFriend() async {
+    await showDialog<String>(
+      context: context,
+      child: new AlertDialog(
+          contentPadding: const EdgeInsets.all(18.0),
+          content: new Row(
+            children: <Widget>[
+              new Expanded(
+                child: new TextField(
+                  controller: ipController,
+                  decoration: new InputDecoration(
+                      labelText: 'Enter your friend\'s ip address',
+                      hintText: 'eg. 199.59.102.200'),
+                ),
+              )
+            ],
+          ),
+          actions: <Widget>[
+            new FlatButton(
+                child: Text("Add Friend"),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+            new FlatButton(
+                child: Text("Cancel"), onPressed: () => Navigator.pop(context)),
+          ]),
+    );
+  }
 }
---
+++ lib/friends_model.dart
@@ -0,0 +1,10 @@
+class Friend {
+  final String name;
+  final String ip;
+
+  Friend({this.name, this.ip});
+}
+
+//Example friends list:
+
+List<Friend> friends = [Friend(name: "Vi", ip: "199.59.102.246")];
