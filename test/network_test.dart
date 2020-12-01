f9a5e55a47bfad9fa77bb1407c5e1e1e668132fe
ericpinter <48108495+ericpinter@users.noreply.github.com>
2020-12-01 11:05:32 -0600

    Test updates
    the database test will need to be run on an actual device (so flutter run ... not flutter test ...) to pull up a real copy of the database.

--- test/network_test.dart
+++ test/network_test.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/widgets.dart' as Widget;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_sharing/database/friends_database.dart';
+import 'package:image_sharing/database/posts_database.dart';
 import 'package:image_sharing/network/friends_model.dart';
 import 'package:image_sharing/network/network.dart';
 import 'package:image_sharing/network/network_utils.dart';
@@ -10,13 +12,14 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   test('Transfer image', () async {
+    Friends.init(persist: false);
     var self = Friend("127.0.0.1");
     var friends = Friends();
     friends.add(self);
     var image_received = Completer();
     var log = await NetworkLog.getLog(() {
       image_received.complete();
-    });
+    }, persist: false);
 
     var picked = PickedFile("../images/test.png");
 
@@ -29,9 +32,9 @@ void main() {
     print(log.feed);
     expect(log.feed.length, 1);
     expect(log.feed[0], isNotNull);
+    print(log.feed[0].image.height);
     expect(
         log.feed[0].image.height,
-        Widget.Image.memory(await picked.readAsBytes())
-            .height); //actually testing the images being the same is hard, but this is close
+        500); //actually testing the images being the same is hard, but this is close
   });
 }
